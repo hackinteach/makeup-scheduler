43e91e71fda9f88673f7499576f8675c62aabfce
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-05-11 22:19:56 +0700

    display information

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -4,6 +4,37 @@
       <v-layout row wrap>
         <v-flex xs-12>
           <p class="display-2">Class Summary</p>
+          <!-- URL -->
+          <p class="display-1">Share this link to your members</p>
+          <a class="headline" :href="url">{{url}}</a>
+          <!-- QR CODE -->
+          <q-r-code :text="url"/>
+          <!-- EMAIL -->
+          <p class="title">Send link via email</p>
+          <v-select
+            v-model="emails"
+            label="Email list"
+            chips
+            tags
+            solo
+            prepend-icon="email"
+            append-icon=""
+            clearable
+          >
+            <template slot="selection" slot-scope="data">
+              <v-chip
+                :selected="data.selected"
+                close
+                @input="remove(data.item)"
+              >
+                <strong>{{ data.item }}</strong>&nbsp;
+              </v-chip>
+            </template>
+          </v-select>
+          <v-btn
+            @click="sendEmail">
+            send
+          </v-btn>
         </v-flex>
       </v-layout>
     </v-container>
@@ -12,10 +43,14 @@
 
 <script>
   import {db} from '../firebase';
+  import QRCode from "vue-qrcode-component";
 
   export default {
+    components: {QRCode},
     data: () => ({
       session: null,
+      url: '',
+      emails: [],
     }),
 
     beforeCreate() {
@@ -30,6 +65,7 @@
           const k = Object.keys(snapshot.val())[0];
           const s = snapshot.val()[k];
           this.session = s;
+          this.url = 'makeup-scheduler.firebaseapp.com/session/' + s.id;
           // console.log(sid, this.session);
         })
         .then(() => {
@@ -37,11 +73,18 @@
             this.$router.push("/");
           }
         })
-
-
     },
 
-    methods: {},
+    methods: {
+      remove (item) {
+        this.chips.splice(this.chips.indexOf(item), 1);
+        this.chips = [...this.chips]
+      },
+
+      sendEmail(){
+
+      }
+    },
   }
 </script>
 
